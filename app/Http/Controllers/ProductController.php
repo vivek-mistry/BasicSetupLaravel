e1df44ee152c22536a08cbf36e53ede20e4b61dd
Vivek Mistry <vive.mistry@codeclinic.in>
2023-05-26 21:29:21 +0530

    code format

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -39,7 +39,7 @@ class ProductController extends Controller
 
     public function update($id, ProductRequest $request)
     {
-        $request_data = $request->except(['_token','proengsoft_jsvalidation']);
+        $request_data = $request->except(['_token', 'proengsoft_jsvalidation']);
         Product::where('id', $id)->update($request_data);
         return redirect()->route('product_list');
     }
@@ -63,13 +63,13 @@ class ProductController extends Controller
             // $extension = $request->import->extension();
 
             // if ($extension == 'xlsx') {
-                $imports = Excel::import($import, $request->file('import')->store('import'));
-                $data['insert_count'] = $import->insert_count;
-                $data['error_count'] = $import->error_count;
+            $imports = Excel::import($import, $request->file('import')->store('import'));
+            $data['insert_count'] = $import->insert_count;
+            $data['error_count'] = $import->error_count;
 
-                $data['validation_errors'] = $import->data_error;
-                $data['true'] = 'true' ;
-                return view('product_import')->with($data);
+            $data['validation_errors'] = $import->data_error;
+            $data['true'] = 'true';
+            return view('product_import')->with($data);
             // } else {
             //     return redirect()->route('product_import');
             // }
@@ -78,12 +78,14 @@ class ProductController extends Controller
             $error_and_data = collect($e->failures());
             $map_error_unique_data = collect();
             $unique_data = $error_and_data->map(function ($array) use ($map_error_unique_data) {
-                $exist  = collect($map_error_unique_data)->where('row', $array->row())->first();
+                $exist = collect($map_error_unique_data)
+                    ->where('row', $array->row())
+                    ->first();
                 if (!isset($exist['row'])) {
                     $value = $array->values();
                     $value['row'] = $array->row();
                     $value['error_messages'] = implode(',', $array->errors());
-                    $map_error_unique_data[] =  $value;
+                    $map_error_unique_data[] = $value;
                 } else {
                     $message = $exist['error_messages'] .= '|' . implode(',', $array->errors());
                     $index = $map_error_unique_data->search(function ($excel_Row) use ($exist, $message) {
